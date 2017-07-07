49708673a3b7e05b52a1dbb39258ec36aec73c34
Ruben Harutyunyan <harutyunyan.ruben@gmail.com>
2017-07-08 01:50:20 +0400

    Updates

--- path---tags-586c7e095e534a3909ec.js
+++ path---tags-586c7e095e534a3909ec.js
@@ -1,2 +1,2 @@
-webpackJsonp([6],{"./node_modules/json-loader/index.js!./.cache/json/tags.json":function(o,n){o.exports={pathContext:{}}}});
+webpackJsonp([4],{"./node_modules/json-loader/index.js!./.cache/json/tags.json":function(o,n){o.exports={pathContext:{}}}});
 //# sourceMappingURL=path---tags-586c7e095e534a3909ec.js.map
