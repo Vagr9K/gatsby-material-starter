7284f3459fc91ccaeada3c2d32589e2877e0134a
Ruben Harutyunyan <harutyunyan.ruben@gmail.com>
2017-07-26 16:26:50 +0400

    Adds a workaround for low quality images imported from markdown.

--- gatsby-config.js
+++ gatsby-config.js
@@ -31,7 +31,7 @@ module.exports = {
           {
             resolve: 'gatsby-remark-images',
             options: {
-              maxWidth: 690,
+              maxWidth: 1200,
             },
           },
           {
