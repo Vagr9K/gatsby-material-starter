52daed83387ecf2c4842e67f4b3636611933338e
Ruben Harutyunyan <harutyunyan.ruben@gmail.com>
2018-07-02 00:33:08 +0400

    Update PostCover to support gatsby-image.

--- src/components/PostCover/PostCover.jsx
+++ src/components/PostCover/PostCover.jsx
@@ -1,19 +1,41 @@
 import React, { Component } from "react";
+import Img from "gatsby-image";
 import "./PostCover.scss";
 
 class PostCover extends Component {
   render() {
-    const { postNode, mobile } = this.props;
-    const post = postNode.frontmatter;
+    const { fileEdges, postNode, mobile } = this.props;
+    const coverHeight = mobile ? 180 : 350;
+    const post = postNode.frontmatter ? postNode.frontmatter : postNode;
+    const coverNodeList = fileEdges.filter(fileNode => {
+      if (fileNode.node.childImageSharp === null) return false;
+      if (fileNode.node.childImageSharp.resolutions.originalName === post.cover)
+        return true;
+
+      return false;
+    });
+
+    if (coverNodeList.length === 1) {
+      return (
+        <Img
+          sizes={coverNodeList[0].node.childImageSharp.sizes}
+          outerWrapperClassName="md-grid md-cell--9 post-cover"
+          style={{ height: coverHeight, width: "100%" }}
+        />
+      );
+    }
+
     /* eslint no-undef: "off" */
-    const cover =
+    const coverURL =
       post.cover.substring(0, 1) === "/"
         ? __PATH_PREFIX__ + post.cover
         : post.cover;
-    const coverHeight = mobile ? 180 : 350;
     return (
       <div
-        style={{ backgroundImage: `url(${cover})`, height: `${coverHeight}px` }}
+        style={{
+          backgroundImage: `url(${coverURL})`,
+          height: `${coverHeight}px`
+        }}
         className="md-grid md-cell--9 post-cover"
       />
     );
