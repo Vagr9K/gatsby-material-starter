85929c594d488c44be5b6d3c1336c2500fb30542
Ruben Harutyunyan <harutyunyan.ruben@gmail.com>
2018-07-02 00:35:42 +0400

    Update tag template with new GraphQL query and PostListing.

--- src/templates/tag.jsx
+++ src/templates/tag.jsx
@@ -7,13 +7,14 @@ export default class TagTemplate extends React.Component {
   render() {
     const { tag } = this.props.pathContext;
     const postEdges = this.props.data.allMarkdownRemark.edges;
+    const fileEdges = this.props.data.allFile.edges;
     return (
       <div className="tag-container">
         <Helmet>
           <title>{`Posts tagged as "${tag}" | ${config.siteTitle}`}</title>
           <link rel="canonical" href={`${config.siteUrl}/tags/${tag}`} />
         </Helmet>
-        <PostListing postEdges={postEdges} />
+        <PostListing postEdges={postEdges} fileEdges={fileEdges} />
       </div>
     );
   }
@@ -22,6 +23,37 @@ export default class TagTemplate extends React.Component {
 /* eslint no-undef: "off" */
 export const pageQuery = graphql`
   query TagPage($tag: String) {
+    allFile {
+      edges {
+        node {
+          id
+          childImageSharp {
+            id
+            resolutions {
+              base64
+              tracedSVG
+              aspectRatio
+              width
+              height
+              src
+              srcSet
+              srcWebp
+              srcSetWebp
+              originalName
+            }
+            internal {
+              contentDigest
+              type
+              owner
+            }
+            sizes(maxWidth: 1240) {
+              ...GatsbyImageSharpSizes
+              originalName
+            }
+          }
+        }
+      }
+    }
     allMarkdownRemark(
       limit: 1000
       sort: { fields: [frontmatter___date], order: DESC }
