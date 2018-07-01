1fa06071453889582d54dc79702fce02b9289242
Ruben Harutyunyan <harutyunyan.ruben@gmail.com>
2018-07-02 00:35:32 +0400

    Update category template with new GraphQL query and PostListing.

--- src/templates/category.jsx
+++ src/templates/category.jsx
@@ -7,19 +7,19 @@ export default class CategoryTemplate extends React.Component {
   render() {
     const { category } = this.props.pathContext;
     const postEdges = this.props.data.allMarkdownRemark.edges;
+    const fileEdges = this.props.data.allFile.edges;
     return (
       <div className="category-container">
         <Helmet>
-          <title>{`Posts in category "${category}" | ${
-            config.siteTitle
-          }`}
+          <title>
+            {`Posts in category "${category}" | ${config.siteTitle}`}
           </title>
           <link
             rel="canonical"
             href={`${config.siteUrl}/categories/${category}`}
           />
         </Helmet>
-        <PostListing postEdges={postEdges} />
+        <PostListing postEdges={postEdges} fileEdges={fileEdges} />
       </div>
     );
   }
@@ -28,6 +28,37 @@ export default class CategoryTemplate extends React.Component {
 /* eslint no-undef: "off" */
 export const pageQuery = graphql`
   query CategoryPage($category: String) {
+    allFile {
+      edges {
+        node {
+          id
+          childImageSharp {
+            id
+            resolutions {
+              base64
+              tracedSVG
+              aspectRatio
+              width
+              height
+              src
+              srcSet
+              srcWebp
+              srcSetWebp
+              originalName
+            }
+            internal {
+              contentDigest
+              type
+              owner
+            }
+            sizes(maxWidth: 1240) {
+              ...GatsbyImageSharpSizes
+              originalName
+            }
+          }
+        }
+      }
+    }
     allMarkdownRemark(
       limit: 1000
       sort: { fields: [frontmatter___date], order: DESC }
