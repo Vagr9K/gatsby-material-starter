ea9f079d76c18e09049e2b7a8b07d2da90c7ce3c
Ruben Harutyunyan <harutyunyan.ruben@gmail.com>
2018-07-02 19:38:44 +0400

    Add dateFromFormat config variable to support custom date formats as frontmatter input.

--- data/SiteConfig.js
+++ data/SiteConfig.js
@@ -13,6 +13,7 @@ module.exports = {
   siteGATrackingID: "UA-47311644-4", // Tracking code ID for google analytics.
   disqusShortname: "https-vagr9k-github-io-gatsby-material-starter", // Disqus shortname.
   postDefaultCategoryID: "Tech", // Default category for posts.
+  dateFromFormat: "YYYY-MM-DD", // Date format used in the frontmatter.
   dateFormat: "DD/MM/YYYY", // Date format for display.
   userName: "Material User", // Username to display in the author segment.
   userTwitter: "", // Optionally renders "Follow Me" in the UserInfo segment.
