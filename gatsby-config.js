0cde2c46885c443d0c5759ebfe2ef74b10d509cc
Ruben Harutyunyan <harutyunyan.ruben@gmail.com>
2018-07-02 18:59:38 +0400

    Use siteTitleShort for PWA homescreen display.

--- gatsby-config.js
+++ gatsby-config.js
@@ -105,7 +105,7 @@ module.exports = {
       resolve: "gatsby-plugin-manifest",
       options: {
         name: config.siteTitle,
-        short_name: config.siteTitle,
+        short_name: config.siteTitleShort,
         description: config.siteDescription,
         start_url: config.pathPrefix,
         background_color: "#e0e0e0",
