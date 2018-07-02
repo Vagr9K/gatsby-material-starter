ebec464136b49347b25b2e2c9042de15e13e294f
Ruben Harutyunyan <harutyunyan.ruben@gmail.com>
2018-07-03 00:08:08 +0400

    Tune gatsby-remark-images maxWidth.

--- gatsby-config.js
+++ gatsby-config.js
@@ -47,7 +47,7 @@ module.exports = {
           {
             resolve: "gatsby-remark-images",
             options: {
-              maxWidth: 1200
+              maxWidth: 672
             }
           },
           {
