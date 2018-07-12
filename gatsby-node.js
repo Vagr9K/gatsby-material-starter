363db706a7f1847098db90c0b01473b641aeacaf
Ruben Harutyunyan <harutyunyan.ruben@gmail.com>
2018-07-12 07:23:57 +0400

    [WORKAROUND] Add babel-polyfill as an import to gatsby-node.

--- gatsby-node.js
+++ gatsby-node.js
@@ -2,6 +2,7 @@ const path = require("path");
 const _ = require("lodash");
 const moment = require("moment");
 const siteConfig = require("./data/SiteConfig");
+require("babel-polyfill");
 
 const postNodes = [];
 
