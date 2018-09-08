fdd81c54d6bc4a8f3b31ad33bd190ff312e36f79
Sergio C. Orozco Torres <scot3004@gmail.com>
2018-09-08 04:18:21 -0500

    Refactor for SEO Component

--- src/components/SEO/index.jsx
+++ src/components/SEO/index.jsx
@@ -1,71 +1,10 @@
 import React, { Component } from "react";
 import Helmet from "react-helmet";
-import urljoin from "url-join";
 import config from "../../../data/SiteConfig";
 
 class SEO extends Component {
   render() {
-    const { postNode, postPath, postSEO } = this.props;
-    let title;
-    let description;
-    let image;
-    let postURL;
-    if (postSEO) {
-      const postMeta = postNode.frontmatter;
-      ({ title } = postMeta);
-      description = postMeta.description
-        ? postMeta.description
-        : postNode.excerpt;
-      image = postMeta.cover;
-      postURL = urljoin(config.siteUrl, config.pathPrefix, postPath);
-    } else {
-      title = config.siteTitle;
-      description = config.siteDescription;
-      image = config.siteLogo;
-    }
-    image = urljoin(config.siteUrl, config.pathPrefix, image);
-    const blogURL = urljoin(config.siteUrl, config.pathPrefix);
-    const schemaOrgJSONLD = [
-      {
-        "@context": "http://schema.org",
-        "@type": "WebSite",
-        url: blogURL,
-        name: title,
-        alternateName: config.siteTitleAlt ? config.siteTitleAlt : ""
-      }
-    ];
-    if (postSEO) {
-      schemaOrgJSONLD.push([
-        {
-          "@context": "http://schema.org",
-          "@type": "BreadcrumbList",
-          itemListElement: [
-            {
-              "@type": "ListItem",
-              position: 1,
-              item: {
-                "@id": postURL,
-                name: title,
-                image
-              }
-            }
-          ]
-        },
-        {
-          "@context": "http://schema.org",
-          "@type": "BlogPosting",
-          url: blogURL,
-          name: title,
-          alternateName: config.siteTitleAlt ? config.siteTitleAlt : "",
-          headline: title,
-          image: {
-            "@type": "ImageObject",
-            url: image
-          },
-          description
-        }
-      ]);
-    }
+    const {title, description, image, url, type, schemaOrgJSONLD} = this.props;
     return (
       <Helmet>
         {/* General tags */}
@@ -78,8 +17,8 @@ class SEO extends Component {
         </script>
 
         {/* OpenGraph tags */}
-        <meta property="og:url" content={postSEO ? postURL : blogURL} />
-        {postSEO ? <meta property="og:type" content="article" /> : null}
+        <meta property="og:url" content={url} />
+        <meta property="og:type" content={type} />
         <meta property="og:title" content={title} />
         <meta property="og:description" content={description} />
         <meta property="og:image" content={image} />
@@ -100,6 +39,13 @@ class SEO extends Component {
       </Helmet>
     );
   }
+}
+
+SEO.defaultProps = {
+  title: config.siteTitle,
+  description: config.siteDescription,
+  image: config.siteLogo,
+  url: config.siteUrl + config.pathPrefix
 }
 
 export default SEO;
