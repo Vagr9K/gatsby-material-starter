c682645ba01e15c88c47e883b4838dff6bba7375
Ruben Harutyunyan <harutyunyan.ruben@gmail.com>
2018-11-07 02:58:59 +0400

    Updates

---
+++ component---node-modules-gatsby-plugin-offline-app-shell-js-62d1a7f18146ee73b1b3.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{151:function(e,t,n){"use strict";var u=n(9);t.__esModule=!0,t.default=void 0;var o=u(n(6)),r=u(n(0)),a=function(e){function t(){return e.apply(this,arguments)||this}return(0,o.default)(t,e),t.prototype.render=function(){return r.default.createElement(r.default.Fragment,null)},t}(r.default.Component);t.default=a}}]);
+//# sourceMappingURL=component---node-modules-gatsby-plugin-offline-app-shell-js-62d1a7f18146ee73b1b3.js.map
--- sw.js
+++ sw.js
@@ -11,8 +11,8 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("workbox-v3.5.0/workbox-sw.js");
-workbox.setConfig({modulePathPrefix: "workbox-v3.5.0"});
+importScripts("workbox-v3.6.3/workbox-sw.js");
+workbox.setConfig({modulePathPrefix: "workbox-v3.6.3"});
 
 workbox.core.setCacheNameDetails({prefix: "gatsby-plugin-offline"});
 
@@ -26,49 +26,17 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-7fe16a61e016e490695a.js"
+    "url": "webpack-runtime-c1817b46845c2db26844.js"
   },
   {
-    "url": "app-bcb3896c41722c7d3899.js"
+    "url": "app-6b1a04579616e872b26b.js"
   },
   {
-    "url": "component---node-modules-gatsby-plugin-offline-app-shell-js-554b9a695eb29cca4f78.js"
-  },
-  {
-    "url": "index.html",
-    "revision": "e82e5a56fc4eaf6bcb7abece350fe862"
+    "url": "component---node-modules-gatsby-plugin-offline-app-shell-js-62d1a7f18146ee73b1b3.js"
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "33626a4900dd1a2e4b0967fd54a11597"
-  },
-  {
-    "url": "3.5b45ca12df3e7aa2ab91.css"
-  },
-  {
-    "url": "1.ec4af2b85f6e8b7b01e2.css"
-  },
-  {
-    "url": "0.3a3ba0677171daf4818c.css"
-  },
-  {
-    "url": "component---src-pages-index-jsx-ca811d37e8c652f4f42a.js"
-  },
-  {
-    "url": "2-f1ac7eed66141fc92f3f.js"
-  },
-  {
-    "url": "0-f538097f4393b4f08fb4.js"
-  },
-  {
-    "url": "3-b41f25f71cd7ec4ffbfb.js"
-  },
-  {
-    "url": "1-b8125e0e7ed92a698369.js"
-  },
-  {
-    "url": "static/d/330/path---index-6a9-LTboCqQsPlfrWi7eZ5fYPneBYE4.json",
-    "revision": "f3188cd5d502625cb2d74583a1218b2f"
+    "revision": "ba6dcb06a84e86c3bd14e56283a8b46a"
   },
   {
     "url": "static/d/520/path---offline-plugin-app-shell-fallback-a-30-c5a-NZuapzHg3X9TaN1iIixfv1W23E.json",
@@ -83,12 +51,12 @@ workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute("/gatsby-material-starter/offline-plugin-app-shell-fallback/index.html", {
-  whitelist: [/^[^?]*([^.?]{5}|\.html)(\?.*)?$/],
+  whitelist: [/^([^.?]*|[^?]*\.([^.?]{5,}|html))(\?.*)?$/],
   blacklist: [/\?(.+&)?no-cache=1$/],
 });
 
 workbox.routing.registerRoute(/\.(?:png|jpg|jpeg|webp|svg|gif|tiff|js|woff|woff2|json|css)$/, workbox.strategies.staleWhileRevalidate(), 'GET');
-workbox.routing.registerRoute(/^https:/, workbox.strategies.networkFirst(), 'GET');
+workbox.routing.registerRoute(/^https?:/, workbox.strategies.networkFirst(), 'GET');
 "use strict";
 
 /* global workbox */
@@ -100,10 +68,19 @@ self.addEventListener("message", function (event) {
     var cacheName = workbox.core.cacheNames.runtime;
     event.waitUntil(caches.open(cacheName).then(function (cache) {
       return Promise.all(resources.map(function (resource) {
-        return cache.add(resource).catch(function (e) {
-          // ignore TypeErrors - these are usually due to
-          // external resources which don't allow CORS
-          if (!(e instanceof TypeError)) throw e;
+        var request; // Some external resources don't allow
+        // CORS so get an opaque response
+
+        if (resource.match(/^https?:/)) {
+          request = fetch(resource, {
+            mode: "no-cors"
+          });
+        } else {
+          request = fetch(resource);
+        }
+
+        return request.then(function (response) {
+          return cache.put(resource, response);
         });
       }));
     }));
