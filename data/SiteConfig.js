f60ee43d5d39d9f28deff2cc8fbf232c55483f08
Ruben Harutyunyan <harutyunyan.ruben@gmail.com>
2019-01-21 02:00:47 +0400

    Add validation to SiteConfig.

--- data/SiteConfig.js
+++ data/SiteConfig.js
@@ -1,4 +1,4 @@
-module.exports = {
+const config = {
   siteTitle: "Gatsby Material Starter", // Site title.
   siteTitleShort: "GM Starter", // Short site title for homescreen (PWA). Preferably should be under 12 characters to prevent truncation.
   siteTitleAlt: "GatsbyJS Material Starter", // Alternative site title for SEO.
@@ -40,3 +40,23 @@ module.exports = {
   ],
   copyright: "Copyright © 2017. Material User" // Copyright string for the footer of the website and RSS feed.
 };
+
+// Validate
+
+// Make sure pathPrefix is empty if not needed
+if (config.pathPrefix === "/") {
+  config.pathPrefix = "";
+} else {
+  // Make sure pathPrefix only contains the first forward slash
+  config.pathPrefix = `/${config.pathPrefix.replace(/^\/|\/$/g, "")}`;
+}
+
+// Make sure siteUrl doesn't have an ending forward slash
+if (config.siteUrl.substr(-1) === "/")
+  config.siteUrl = config.siteUrl.slice(0, -1);
+
+// Make sure siteRss has a starting forward slash
+if (config.siteRss && config.siteRss[0] !== "/")
+  config.siteRss = `/${config.siteRss}`;
+
+module.exports = config;
