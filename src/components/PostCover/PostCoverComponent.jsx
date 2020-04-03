1914192fd1a4d6b32e2e58d28a29787ec066584a
Ruben Harutyunyan <harutyunyan.ruben@gmail.com>
2020-04-03 18:47:26 +0400

    Fix image paths trough NetlifyCMS creating issues. Based on the fix proposed in #69 by @felixlberg.

--- src/components/PostCover/PostCoverComponent.jsx
+++ src/components/PostCover/PostCoverComponent.jsx
@@ -17,6 +17,9 @@ class PostCover extends Component {
       )
         return true;
 
+      if (fileNode.node.absolutePath.indexOf(path.join("/", post.cover)) !== -1)
+        return true;
+
       return false;
     });
 
